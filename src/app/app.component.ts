bde8b462177c8ec677ea0ad653b5e4b641b2a77e
Mateusz Cwojdziński <mateuszcwojdzinski@outlook.com>
2024-11-07 22:09:42 +0100

    Add finished excersise for angular front-end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { StudentsComponent } from './students/students.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, StudentsComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'StudentsFront';
+  title = 'Students';
 }
---
+++ src/app/students/students.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { StudentService } from '../student.service';
+import { Student } from '../student';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-students',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './students.component.html',
+  styleUrl: './students.component.css'
+})
+export class StudentsComponent {
+  constructor(private studentService: StudentService) {}
+  students!: Student[];
+
+  getStudents(): void {
+    this.studentService.getStudents()
+    .subscribe(students => this.students = students);
+    }
+
+    create(index: number, firstName: string, lastName: string): void {
+      this.studentService.createStudent(
+      new Student( index, firstName, lastName))
+      .subscribe(student => { this.students.push(student); });
+      }
+
+      delete(student: Student): void {
+        this.students = this.students.filter(s => s.id !== student.id);
+        this.studentService.deleteStudent(student).subscribe();
+        }
+
+  ngOnInit() {
+    this.getStudents();
+  }
+}
